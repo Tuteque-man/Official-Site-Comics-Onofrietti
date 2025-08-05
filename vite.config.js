aca2edbb4ed6705721ea81ad4ffa9b97645725f1
Onox2099 <angelloxon@gmail.com>
2025-08-05 11:40:52 -0400

    Add gh-pages deployment configuration

--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
+  base: '/Official-Site-Comics-Onofrietti/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
